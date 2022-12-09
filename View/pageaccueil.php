88ce0668ddbb9de52c813c269ab4323786d7d7a6
IbraTinou <gatboismartin@gmail.com>
2022-12-09 12:39:52 +0100

    page accueil
    ajout

---
+++ View/pageaccueil.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="pageacueil.css">
+    <title>page d'acceuil</title>
+    </head>
+    <body>
+        <div class="bandeau">
+            
+
+        <h1>Présentation du site</h1>
+        <p> Notre site est destinés à la présentation de nos valeurs, de nos engagements, et bien sur de nos produits et nos offres.
+            Il est fait main du début à la fin par une équipe francaise qui représente notre engagement écologique, de plus
+            , il offre une option FAQ pour que vous puissiez nous communiquer vos remarques constructives ou des question 
+            technique quant à l'utilisation de nos produits.
+             </p>
+            
+            </div>
+       <div class="produit">
+        <a class="button" href="#">Achetez maintenant</a>
+        <a href="#">
+            <img class="bbb" src="https://www.ravate.com/68488/casque-audio-bose-qc35-ii-noir.jpg" 
+            alt="photo casque">
+            
+        </a>
+        </div>
+        <div class="bandeau">
+            <h1>Présentation du produit</h1>
+            <p>Notre produit phare se nomme ANDREA, c'est un casque high-tech offrant plusieurs options permettant
+                de vous accompagner au mieux lors de vos sessions de sports, ce casque est notament équipé d'un capteur
+                cardiaque, d'un capteurs de température et un capteur de CO2.
+            </p>
+        </div>
+            <h1 class="T3">Présentation des fonctionalité du site et du produit</h1>
+            <p>paragraphe....
+                ...............................................................................................................
+                ........................................................................................................
+                ......................................................................................................
+                .........................................................................................................</p>
+
+    
+    </body>
+</html>
+
+
