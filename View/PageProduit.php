e50d36e8a0499e7734662fa912374901ddf1683b
Alex <alexisjacquet9@gmail.com>
2022-12-12 01:09:46 +0100

    Changement de la police en Oufit
    Changement de la police des textes

--- View/PageProduit.php
+++ View/PageProduit.php
@@ -44,7 +44,7 @@
         </div>
     </div>
 
-    <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+    <div style="font-family: 'Outfit', sans-serif; color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
         <h3 style="text-align:center;">Devise de l'entreprise</h3>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate sed tenetur asperiores illum, eos incidunt fuga in doloribus ullam. Quos id corporis eveniet ea saepe velit et, praesentium vel architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum minus, optio sint ea similique a veniam quam autem doloremque vel ad aliquam pariatur quod, quia, blanditiis molestias dolorum fugit architecto!
         </p>
@@ -52,24 +52,24 @@
 
     <div class="parallax2">
         <div class="texteparallax">
-            <span class="contourtexteparallax" style="background-color:transparent;font-size:25px;color: #f7f7f7;">100% MADE IN FRANCE</span>
+            <span class="contourtexteparallax" style="font-family: 'Outfit', sans-serif; background-color:transparent;font-size:25px;color: #f7f7f7;">100% MADE IN FRANCE</span>
         </div>
     </div>
 
     <div style="position:relative;">
-        <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+        <div style="font-family: 'Outfit', sans-serif; color:black;background-color:#7FC0C4;text-align:center;padding:50px 80px;text-align: justify;">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, amet ab? Iste doloremque est explicabo, fugiat facere accusantium.</p>
         </div>
     </div>
 
     <div class="parallax3">
         <div class="texteparallax">
-            <span class="contourtexteparallax" style="background-color:transparent;font-size:25px;color: #f7f7f7;">DECOUVREZ NOTRE PRODUIT ICI</span>
+            <span class="contourtexteparallax" style="font-family: 'Outfit', sans-serif; background-color:transparent;font-size:25px;color: #f7f7f7;">DECOUVREZ NOTRE PRODUIT ICI</span>
         </div>
     </div>
 
     <div style="position:relative;">
-        <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+        <div style="font-family: 'Outfit', sans-serif; color:black;background-color:#7FC0C4;text-align:center;padding:50px 80px;text-align: justify;">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, amet ab? Iste doloremque est explicabo, fugiat facere accusantium.</p>
         </div>
     </div>
@@ -77,13 +77,13 @@
 
     <div class="parallax4">
         <div class="texteparallax">
-            <span class="contourtexteparallax" style="background-color:transparent;font-size:25px;color: #f7f7f7;">RESPIREZ ENFIN DE L'AIR PUR EN COURANT</span>
+            <span class="contourtexteparallax" style="font-family: 'Outfit', sans-serif; background-color:transparent;font-size:25px;color: #f7f7f7;">RESPIREZ ENFIN DE L'AIR PUR EN COURANT</span>
         </div>
     </div>
 
 
     <div style="position:relative;">
-        <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+        <div style="font-family: 'Outfit', sans-serif; color:black;background-color:#7FC0C4;text-align:center;padding:50px 80px;text-align: justify;">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, amet ab? Iste doloremque est explicabo, fugiat facere accusantium.</p>
         </div>
     </div>
@@ -92,7 +92,7 @@
  <!-- VIDEO -->
 
  <div class="myVideo">
-    <div class="textevideo">
+    <div class="textevideo" style="font-family: 'Outfit', sans-serif;">
         <h5>Pour un son authentique</h5>
      </div>
     <video autoplay loop muted>
