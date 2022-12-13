b104715602a229c7b8e59259f8ed2f9de2e55c03
IbraTinou <gatboismartin@gmail.com>
2022-12-13 15:07:38 +0100

    ajout page accueil
    ajout page accueil

--- View/pageaccueil.php
+++ View/pageaccueil.php
@@ -1,45 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-    <link rel="stylesheet" href="pageacueil.css">
-    <title>page d'acceuil</title>
-    </head>
-    <body>
-        <div class="bandeau">
-            
-
-        <h1>Présentation du site</h1>
-        <p> Notre site est destinés à la présentation de nos valeurs, de nos engagements, et bien sur de nos produits et nos offres.
-            Il est fait main du début à la fin par une équipe francaise qui représente notre engagement écologique, de plus
-            , il offre une option FAQ pour que vous puissiez nous communiquer vos remarques constructives ou des question 
-            technique quant à l'utilisation de nos produits.
-             </p>
-            
-            </div>
-       <div class="produit">
-        <a class="button" href="#">Achetez maintenant</a>
-        <a href="#">
-            <img class="bbb" src="https://www.ravate.com/68488/casque-audio-bose-qc35-ii-noir.jpg" 
-            alt="photo casque">
-            
-        </a>
-        </div>
-        <div class="bandeau">
-            <h1>Présentation du produit</h1>
-            <p>Notre produit phare se nomme ANDREA, c'est un casque high-tech offrant plusieurs options permettant
-                de vous accompagner au mieux lors de vos sessions de sports, ce casque est notament équipé d'un capteur
-                cardiaque, d'un capteurs de température et un capteur de CO2.
-            </p>
-        </div>
-            <h1 class="T3">Présentation des fonctionalité du site et du produit</h1>
-            <p>paragraphe....
-                ...............................................................................................................
-                ........................................................................................................
-                ......................................................................................................
-                .........................................................................................................</p>
-
+<head>
+<title>flexboxtrye</title>
+<link rel="stylesheet" href="../pageaccueil.css">
+</head>
+<body>
     
-    </body>
+<header class="header">
+    <h1>Présentation du site</h1>
+    <p>Notre site est destinés à la présentation de nos valeurs, de nos engagements, et bien sur de nos produits et nos offres.
+        Il est fait main du début à la fin par une équipe francaise qui représente notre engagement écologique, de plus
+        , il offre une option FAQ pour que vous puissiez nous communiquer vos remarques constructives ou des question 
+        technique quant à l'utilisation de nos produits.</p>
+</header>
+<div class="content">
+    <a href="#" class="bloc1">ACHETEZ <div class="maintenant">MAINTENANT</div> </a>
+    <div class="bloc2">
+        <h2>Présentation du produit</h2>
+        <p>Notre produit phare se nomme ANDREA, c'est un casque high-tech offrant plusieurs options permettant
+            de vous accompagner au mieux lors de vos sessions de sports, ce casque est notament équipé d'un capteur
+            cardiaque, d'un capteurs de température et un capteur de CO2.</p>
+     </div>
+    <div class="bloc3"> 
+        <a href="#"><img class="image" src="https://www.ravate.com/68488/casque-audio-bose-qc35-ii-noir.jpg" alt="image"></a>
+    </div>
+</div>
+    
+<footer class="footer">
+    <h1>Présentation des fonctionalités du site et du produit</h1>
+    <p>Notre site est destinés à la présentation de nos valeurs, de nos engagements, et bien sur de nos produits et nos offres.
+        Il est fait main du début à la fin par une équipe francaise qui représente notre engagement écologique, de plus
+        , il offre une option FAQ pour que vous puissiez nous communiquer vos remarques constructives ou des question 
+        technique quant à l'utilisation de nos produits.
+        Notre produit phare se nomme ANDREA, c'est un casque high-tech offrant plusieurs options permettant
+        de vous accompagner au mieux lors de vos sessions de sports, ce casque est notament équipé d'un capteur
+        cardiaque, d'un capteurs de température et un capteur de CO2.
+    </p>
+</footer>
+</body>
 </html>
-
-
