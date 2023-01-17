b3b23afa67a21ad17ae7518230cb17cdd78804ba
Finalblue <coco.michoumineur@gmail.com>
2023-01-17 09:41:12 +0100

    Update chatAdmin.php

--- View/chatAdmin.php
+++ View/chatAdmin.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="Admin.css">
 </head>
 <body>
-<?php
+    <?php
      include("View/Header.php");
     ?>
 
@@ -16,6 +16,9 @@
                 <p>Titre de la conversation</p>
             </div>
             <div class="container-bulles scroller">
+                <?php
+
+                ?>
                 <div class="bubble-text-left">
                     <p>Blablabla</p>
                 </div>
@@ -35,9 +38,18 @@
                     <p>Blablablazf,efnefnefneienfeinfeifnefuebeufhufheneiufneiufenuienfeiufeufebfyujebfeufhehfuj</p>
                 </div>  
             </div>
-            <input class="input-send-msg" type="text" placeholder="Type your message here ...">
+            <input class="input-send-msg"  id="sendmsg" type="text" placeholder="Type your message here ...">
         </div>
     </div>
+    <script>
+        document.getElementById("sendmsg").onkeydown = function(e){
+            if(e.keyCode == 13){
+                console.log("SUUUU");
+            }
+
+        }
+
+    </script>
 
 
     <?php
